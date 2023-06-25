f2a94735f0bb7027c4248d758574a068c95eb21e
Shaunax1 <120845915+Shaunax1@users.noreply.github.com>
2023-06-25 18:13:59 +0300

    Update linear-regression.py

--- linear-regression.py
+++ linear-regression.py
@@ -1,33 +1,26 @@
 import pandas as pd 
 import matplotlib.pyplot as plt
-from sklearn.datasets import load_iris
 
-iris = load_iris()
-data = pd.DataFrame(iris.data)
-
-data.head()
-data.columns = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
-
-# 0 = Iris-setosa 1 = Iris-versicolor 2 = Iris-virginica
-target = pd.DataFrame(iris.target)
-target = target.rename(columns = {0: 'target'})
-target.head()
-data = pd.concat([data, target], axis = 1)
-
+data.columns =["id","sepal_length","sepal_width","petal_length","petal_width","species"]
+data.drop(["id"],axis=1)
 data.isnull().sum()
+
+#plot
+plt.scatter(data.sepal_length,data.petal_length)
+plt.xlabel("x")
+plt.ylabel("y")
+plt.show()
 
 #%%
 from sklearn.linear_model import LinearRegression
-
 lr = LinearRegression()
 
 x = data.iloc[:,0].values.reshape(-1,1)
-y = data.iloc[:,2].values.reshape(-1,1)
+y = data.iloc[:,1].values.reshape(-1,1)
 
 lr.fit(x,y)
-
 y_head = lr.predict(x)
-plt.plot(x,y_head,color="red")
 
 from sklearn.metrics import r2_score
 print("r2 : " , r2_score(y,y_head))
+
