2b0b42b665fda638717257844d70b4cda13df6de
Shaunax1 <120845915+Shaunax1@users.noreply.github.com>
2023-06-25 18:20:45 +0300

    Update multiple-linear-regression.py

--- multiple-linear-regression.py
+++ multiple-linear-regression.py
@@ -1,34 +1,28 @@
 import pandas as pd 
 import matplotlib.pyplot as plt
-import numpy as np
 
-df = pd.read_csv("Mall_Customers.csv", sep=";")
+data.columns =["id","sepal_length","sepal_width","petal_length","petal_width","species"]
+data.drop(["id"],axis=1)
+data["species"].replace(["Iris-setosa" , "Iris-versicolor" , "Iris-virginica"], [1,2,3])
+data.isnull().sum()
 
-df.head()
-df.describe()
-
-df.rename( columns = {"Annual Income (k$)" : "annual_income",
-                      "Spending Score (1-100)" : "spending_score"
-                      }, inplace=True)
-
-plt.scatter(df.Age,df.annual_income,df.spending_score)
-plt.xlabel("spending-score,Age")
-plt.ylabel("annual_income")
+#plot
+plt.scatter(data.sepal_length,data.petal_length)
+plt.xlabel("x")
+plt.ylabel("y")
 plt.show()
 
-
 from sklearn.linear_model import LinearRegression
-multipleLinear = LinearRegression()
+mlr = LinearRegression()
 
-x = df.iloc[:,[3,4]].values
-y = df.iloc[:,2]
+x = data.iloc[:,[0,1]].values
+y = data.iloc[:,4]
 
-multipleLinear.fit(x,y)
-
-y_head = multipleLinear.predict(x)
-print("b0 : " , multipleLinear.intercept_)
-print("b1,b2 : " , multipleLinear.coef_)
-
+mlr.fit(x,y)
+y_head2 = mlr.predict(x)
 
 from sklearn.metrics import r2_score
-print("r^2 score : ", r2_score(y,y_head))
+print("r2 : " , r2_score(y,y_head2))
+
+
+
