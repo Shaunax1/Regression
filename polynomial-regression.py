0863255ee38fc1009e26c10b0d639173057c303e
Shaunax1 <120845915+Shaunax1@users.noreply.github.com>
2023-06-25 18:26:05 +0300

    Update polynomial-regression.py

--- polynomial-regression.py
+++ polynomial-regression.py
@@ -1,38 +1,24 @@
 import pandas as pd 
 import matplotlib.pyplot as plt
-from sklearn.datasets import load_iris
 
-iris = load_iris()
-data = pd.DataFrame(iris.data)
-
-data.head()
-data.columns = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
-
-# 0 = Iris-setosa 1 = Iris-versicolor 2 = Iris-virginica
-target = pd.DataFrame(iris.target)
-target = target.rename(columns = {0: 'target'})
-target.head()
-data = pd.concat([data, target], axis = 1)
-
+data.columns =["id","sepal_length","sepal_width","petal_length","petal_width","species"]
+data.drop(["id"],axis=1)
+data["species"].replace(["Iris-setosa" , "Iris-versicolor" , "Iris-virginica"], [1,2,3])
 data.isnull().sum()
+
+from sklearn.preprocessing import PolynomialFeatures
+plr = PolynomialFeatures(degree = 5)
 
 x = data.iloc[:,0].values.reshape(-1,1)
 y = data.iloc[:,2].values.reshape(-1,1)
-
-plt.scatter(x,y, color="red")
-plt.xlabel("sepal-length")
-plt.ylabel("petal-length")
-plt.show()
-
-from sklearn.preprocessing import PolynomialFeatures
-plr = PolynomialFeatures(degree = 10)
 x_polynomial = plr.fit_transform(x)
 
 from sklearn.linear_model import LinearRegression
 lr = LinearRegression()
 lr.fit(x_polynomial,y)
 y_head = lr.predict(x_polynomial)
-plt.plot(x,y_head,color="blue")
+plt.plot(a,y_head, color="green" , label="poly")
+plt.legend()
 plt.show()
 
 from sklearn.metrics import r2_score
